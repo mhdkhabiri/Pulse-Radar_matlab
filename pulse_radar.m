43af35bed749e6fe240f85e809dde802f546b8bb
mhdkhabiri <94365467+mhdkhabiri@users.noreply.github.com>
2024-03-31 00:35:18 -0700

    Update pulse_radar.m

--- pulse_radar.m
+++ pulse_radar.m
@@ -2,27 +2,27 @@ time_step = 5e-9;
 PRI = 1e-3;
 N_shift = 5e-4;
 pulse_width = 1e-6;
-NR = 2e6;
-Ts = 1/NR;
+NR = 100;
+%   NR = 2e6;
+%   Ts = 1/NR;
 
 t = 0: time_step: 10*PRI - time_step;
 
 T_signal = SignalProcess(0, t, pulse_width, PRI);
 R_signal = SignalProcess(N_shift, t, pulse_width, PRI);
-ADC_signal = Sampling(R_signal, t, Ts, time_step);
+[t2, ADC_signal] = Sampling(R_signal, NR, time_step);
 
 
-subplot(2, 1, 1)
+subplot(3, 1, 1)
 plot(t, T_signal);
 title('transfer signal'), xlabel('time'), ylabel('amplitude');
 
-subplot(2, 1, 2)
+subplot(3, 1, 2)
 plot(t, R_signal);
 title('receiver signal'), xlabel('time'), ylabel('amplitude');
-
-
+ 
 subplot(3, 1, 3)
-plot(t, ADC_signal)
+plot(t2, ADC_signal)
 title('sampling signal'), xlabel('time'), ylabel('amplitude');
 
 
@@ -41,4 +41,12 @@ end
 end
 
 
-function [output_signal] = sampling ( input_signal, 
+function [t2, output_signal] = Sampling ( input_signal, NR, time_step)
+
+output_signal = resample( input_signal, 1, NR);
+time = time_step * NR;
+t2 = 0: time: (length(input_signal) - time) * time_step;
+
+end
+
+
